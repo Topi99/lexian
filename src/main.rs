8f65531548250799775e2e78d30a5fe2bac66c3e
Topiltzin Hernández Mares <topiltzin.hernandez@wizeline.com>
2021-09-08 17:26:00 -0500

    feat: now it reads from stdin

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,30 @@
-fn main() {
-    println!("Hello, world!");
+use std::io::{self, BufRead};
+
+fn main() -> io::Result<()> {
+    let prods = read_productions();
+
+    println!("{:?}", prods);
+
+    Ok(())
+}
+
+fn read_productions() -> Vec<String> {
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines();
+    let mut prods = Vec::new();
+
+    while let Some(line) = lines.next() {
+        let length: i32 = line.unwrap().trim().parse().unwrap();
+
+        for _ in 0..length {
+            let line = lines
+                .next()
+                .expect("No hubo una siguiente producción")
+                .expect("¡Error al leer la producción!");
+
+            prods.push(line);
+        }
+    }
+
+    prods
 }
