f3e71ed62ad45ba224b28b2aff57dcb84e4561f0
Topiltzin Hernández Mares <topiltzin.hernandez@wizeline.com>
2021-09-08 18:48:00 -0500

    feat: split left and right symbols

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,68 @@
 use std::io::{self, BufRead};
 
+struct Sides {
+    left: Vec<String>,
+    right: Vec<String>,
+}
+
 fn main() -> io::Result<()> {
     let prods = read_productions();
 
-    println!("{:?}", prods);
+    let Sides { left, right } = split_sides(prods);
+
+    println!("{:?} {:?}", left, right);
 
     Ok(())
 }
 
-fn read_productions() -> Vec<String> {
+/// Lee de `stdin` las producciones de la gramática libre de contexto.
+/// La entrada debe empezar con la cantidad de producciones a leer.
+/// Las producciones deben estar en la 
+/// [forma normal de Chomsky](https://en.wikipedia.org/wiki/Chomsky_normal_form)
+/// 
+/// # Ejemplo
+/// 
+/// ```txt
+/// 5
+/// goal -> A
+/// A -> ( A )
+/// A -> two
+/// two -> a
+/// two -> b
+/// ```
+pub fn read_productions() -> Vec<String> {
     let stdin = io::stdin();
     let mut lines = stdin.lock().lines();
     let mut prods = Vec::new();
 
     while let Some(line) = lines.next() {
+        // Se obtiene la cantidad de producciones a leer
         let length: i32 = line.unwrap().trim().parse().unwrap();
 
+        // Se leen las producciones línea a línea
         for _ in 0..length {
             let line = lines
                 .next()
                 .expect("No hubo una siguiente producción")
                 .expect("¡Error al leer la producción!");
 
+            // Se guarda la producción leída
             prods.push(line);
         }
     }
 
     prods
+}
+
+fn split_sides(productions: Vec<String>) -> Sides {
+    let mut left = vec![];
+    let mut right = vec![];
+
+    for production in productions {
+        let splited = production.split(" -> ").collect::<Vec<_>>();
+        left.push(String::from(splited[0]));
+        right.push(String::from(splited[1]));
+    }
+
+    Sides { left, right }
 }
