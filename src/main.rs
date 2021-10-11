fd2658645742721ca345853305e64418c9d2f3ec
Topiltzin Hernández Mares <topiltzin.hernandez@wizeline.com>
2021-10-10 20:58:57 -0500

    feat: find first of a non terminal element

--- src/main.rs
+++ src/main.rs
@@ -14,20 +14,23 @@ struct Sides {
 /// Este es el algoritmo en su mas alto nivel:
 /// 
 /// 1. Se leen las producciones línea por línea de un archivo.
-/// 2. Se se extraen los diferentes lados de las producciones: derecho e izquierdo.
+/// 2. Se se extraen los diferentes lados de las producciones: derecho e
+/// izquierdo.
 /// 3. Del lado izquierdo se obtienen los elementos no terminales.
-/// 4. Del lado derecho se eliminan elementos no terminales y se obtienen terminales.
+/// 4. Del lado derecho se eliminan elementos no terminales y se obtienen
+/// terminales.
 /// 5. Se imprimen elementos terminales y no terminales en la consola.
 fn main() -> io::Result<()> {
     let productions = read_productions();
 
     let Sides { left, right } = split_sides(productions);
 
-    let non_terminals = get_non_terminals(left);
-    let terminals = get_terminals(&non_terminals, right);
+    let non_terminals = get_non_terminals(&left);
+    let terminals = get_terminals(&non_terminals, &right);
 
-    println!("Terminal: {}", terminals.join(", "));
-    println!("Non terminal: {}", non_terminals.join(", "));
+    println!("{:?}", get_first(
+        &String::from("F"), &left, &right
+    ));
 
     Ok(())
 }
@@ -71,11 +74,11 @@ pub fn read_productions() -> Vec<String> {
     prods
 }
 
-/// Transforma un vector de producciones en dos vectores, uno para el
-/// lado derecho y otro para el izquierdo.
+/// Transforma un vector de producciones en dos vectores, uno para el lado
+/// derecho y otro para el izquierdo.
 /// 
-/// `split_sides()` regresa una estructura `Sides` con los diferentes
-/// lados de las producciones.
+/// `split_sides()` regresa una estructura `Sides` con los diferentes lados
+///  de las producciones.
 fn split_sides(productions: Vec<String>) -> Sides {
     let mut left = vec![];
     let mut right = vec![];
@@ -89,9 +92,9 @@ fn split_sides(productions: Vec<String>) -> Sides {
     Sides { left, right }
 }
 
-/// Filtra el lado izquierdo de la gramática y regresa un vector con
-/// todos los elementos no terminales.
-fn get_non_terminals(left_side: Vec<String>) -> Vec<String> {
+/// Filtra el lado izquierdo de la gramática y regresa un vector con todos los
+/// elementos no terminales.
+fn get_non_terminals(left_side: &Vec<String>) -> Vec<String> {
     // Se usa un Vector<String> en lugar de HasSet<String> porque el
     // HashSet<String> ordena de diferente manera sus elementos.
     // Se quieren obtener los elementos en orden de aparición.
@@ -99,8 +102,8 @@ fn get_non_terminals(left_side: Vec<String>) -> Vec<String> {
 
     for element in left_side {
         // La condición es para obtener solamente elementos únicos.
-        if !non_terminals.contains(&element) {
-            non_terminals.push(element);
+        if !non_terminals.contains(element) {
+            non_terminals.push(String::from(element));
         }
     }
 
@@ -110,7 +113,7 @@ fn get_non_terminals(left_side: Vec<String>) -> Vec<String> {
 /// Filtra los elementos terminales desde el lado derecho con ayuda
 /// de los no terminales.
 fn get_terminals(
-    non_terminals: &Vec<String>, right_side: Vec<String>
+    non_terminals: &Vec<String>, right_side: &Vec<String>
 ) -> Vec<String> {
     let mut splited_right = vec![];
     // Se usa un Vector<String> en lugar de HasSet<String> porque el
@@ -118,8 +121,8 @@ fn get_terminals(
     // Se quieren obtener los elementos en orden de aparición.
     let mut terminals = vec![];
 
-    // Ya que algunos elementos del lado derecho pueden venir algo
-    // parecido a esto: `( A )`, debemos volver a separar por espacios
+    // Ya que algunos elementos del lado derecho pueden venir algo parecido
+    // a esto: `( A )`, debemos volver a separar por espacios
     // todos los elementos. 
     for line in right_side {
         for element in line.split(" ") {
@@ -127,8 +130,8 @@ fn get_terminals(
         }
     }
 
-    // Al final se filtran elementos que sean `'` (ya que representan
-    // el símbolo Epsilon) o cualquier no terminal.
+    // Al final se filtran elementos que sean `'` (ya que representan el
+    // símbolo Epsilon) o cualquier no terminal.
     // Solo se mantienen elementos únivos en el vector.
     for element in splited_right {
         if element != "'" && !non_terminals.contains(&element) && !terminals.contains(&element) {
@@ -137,4 +140,44 @@ fn get_terminals(
     }
 
     terminals
+}
+
+fn get_first(
+    non_terminal: &String, left_side: &Vec<String>, right_side: &Vec<String>,
+) -> Vec<String> {
+    let indexes = get_indexes(non_terminal, left_side);
+
+    let mut first = vec![];
+
+    for index in indexes {
+        let first_in_body = &right_side[index]
+            .chars().next().unwrap().to_string();
+
+        match first_in_body.as_str() {
+            "'" => first.push(String::from("' '")),
+            _ => {
+                if left_side.contains(first_in_body) {
+                    first.extend(
+                        get_first(first_in_body, left_side, right_side)
+                    );
+                } else {
+                    first.push(String::from(first_in_body));
+                }
+            }
+        }
+    }
+
+    first
+}
+
+fn get_indexes(terminal: &String, terminals: &Vec<String>) -> Vec<usize> {
+    let mut indexes = vec![];
+
+    for (index, value) in terminals.iter().enumerate() {
+        if terminal == value {
+            indexes.push(index)
+        }
+    }
+
+    indexes
 }
